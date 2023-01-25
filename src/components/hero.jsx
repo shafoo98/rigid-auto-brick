f6d6f7f02cf9614ef7a5002bc8dd025d94fee6bb
shafoo98 <shafinul98@protonmail.com>
2023-01-25 21:33:15 +0600

    Made small changes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./hero.css";
 
 import Typed from "react-typed";
 
 const Hero = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div>
       <div
-        id="background-image"
         className="text-6xl md:text-7xl text-center max-w-[1240px] w-full h-screen mx-auto flex justify-around items-center"
         loading="lazy"
+        onClick={() => navigate("/gallery")}
       >
-        <Typed className="text-[#292020]"
+        <Typed
+          className="text-[#292020]"
           strings={[
-            "Thinking Of Your Dream Infrastructure?",
-            "Think Quality!",
-            "Think Best Prices!",
-            "Think Of Us!",
             "RIGID AUTO BRICKS",
+            "Quality Is Our Only Priority",
+            "Click Here To See Our Products",
           ]}
           loop
           typeSpeed={40}
